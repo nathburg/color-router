e5ff7b65edfa772d4f449c5302100d27fc14e0f7
Nathan Burgess <nathans.email.address@gmail.com>
2022-09-27 13:21:40 -0700

    improved NotFound component to show up whenever path is not correct

--- src/Components/Color Display/ColorDisplay.js
+++ src/Components/Color Display/ColorDisplay.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import Header from '../Header/Header';
+import NotFound from '../NotFound/NotFound';
 import './ColorDisplay.css';
+
+function inRange(number) {
+  return 0 <= number && number < 256;
+}  
 
 export default function ColorDisplay() {
   const { r, g, b } = useParams();
-  return (
-    <>  
-      <Header />
-      <div className='color-display' style={{ background: `rgb(${r}, ${g}, ${b})` }}>
-        <div className='rgb'>
-          rgb({r}, {g}, {b})
+
+  if (inRange(r) && inRange(g) && inRange(b)) {
+    return (
+      <>  
+        <Header />
+        <div className='color-display' style={{ background: `rgb(${r}, ${g}, ${b})` }}>
+          <div className='rgb'>
+            rgb({r}, {g}, {b})
+          </div>
         </div>
-      </div>
-    </>  
-  );
+      </>  
+    );
+  } else {
+    return <NotFound />; 
+  }
 }
