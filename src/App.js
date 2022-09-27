350ceb845e8acbd68794b7f6feec6f9874367f3f
Nathan Burgess <nathans.email.address@gmail.com>
2022-09-27 14:04:54 -0700

    added default NotFound route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Main from './Components/Main/Main';
 import Home from './Components/Home/Home';
+import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App"> 
       <Switch>
         <Route path='/rgb/:r/:g/:b' component={Main} />
-        <Route path='/' component={Home} />
+        <Route exact path='/' component={Home} />
+        <Route path='*' component={NotFound} />
       </Switch>
     </div>
   );
