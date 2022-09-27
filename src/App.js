60fc7ca4a1b6d93ac16c1b3bbfede41460e8c16a
Nathan Burgess <nathans.email.address@gmail.com>
2022-09-27 14:25:16 -0700

    refactored to get rid of Home component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Main from './Components/Main/Main';
-import Home from './Components/Home/Home';
 import NotFound from './Components/NotFound/NotFound';
 
 function App() {
@@ -9,7 +8,9 @@ function App() {
     <div className="App"> 
       <Switch>
         <Route path='/rgb/:r/:g/:b' component={Main} />
-        <Route exact path='/' component={Home} />
+        <Route exact path='/'>
+          <Redirect to='/rgb/0/0/0' /> 
+        </Route>
         <Route path='*' component={NotFound} />
       </Switch>
     </div>
