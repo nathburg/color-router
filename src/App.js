db60eda9cc56a4b7f98d108ea1e7621e91ff69d8
Nathan Burgess <nathans.email.address@gmail.com>
2023-01-09 22:46:40 -0800

    React Router 6 works

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Main from './Components/Main/Main';
 import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   return (
-    <div className="App"> 
-      <Switch>
-        <Route path='/rgb/:r/:g/:b' component={Main} />
-        <Route exact path='/'>
-          <Redirect to='/rgb/0/0/0' /> 
-        </Route>
-        <Route path='*' component={NotFound} />
-      </Switch>
+    <div className="App">
+      <Routes>
+        <Route path="/rgb/:r/:g/:b" element={<Main />} />
+        <Route path="/" element={<Navigate to="/rgb/0/0/0" />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
