4186afa2ecdc22d4b2a7c79048244de8d85ba027
Nathan Burgess <nathans.email.address@gmail.com>
2023-01-23 12:13:29 -0800

    fixes activeLink coloring

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,25 +3,32 @@ import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 export default function Header() {
-  
   return (
-    <div className='header'>
+    <div className="header">
       <div>
-        <NavLink to='/rgb/0/71/171' activeStyle={{ color: 'rgb(0,71,171' }}>
+        <NavLink
+          to="/rgb/0/71/171"
+          style={({ isActive }) => (isActive ? { color: 'rgb(0,71,171' } : { color: 'gray' })}
+        >
           Cobalt Blue
         </NavLink>
       </div>
       <div>
-        <NavLink to='/rgb/150/64/0' activeStyle={{ color: 'rgb(150, 64, 0' }}>
+        <NavLink
+          to="/rgb/150/64/0"
+          style={({ isActive }) => (isActive ? { color: 'rgb(150, 64, 0' } : { color: 'gray' })}
+        >
           Burnt Orange
         </NavLink>
       </div>
       <div>
-        <NavLink to='/rgb/255/184/212' activeStyle={{ color: 'rgb(255, 184, 212)' }}>
+        <NavLink
+          to="/rgb/255/184/212"
+          style={({ isActive }) => (isActive ? { color: 'rgb(255, 184, 212)' } : { color: 'gray' })}
+        >
           Pepto Bismol
         </NavLink>
       </div>
     </div>
   );
 }
-  
